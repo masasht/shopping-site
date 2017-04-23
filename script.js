f4e35484de63866de77cc05c911f9a576ae17fbc
Takano Masashi <masasht@gmail.com>
2017-04-23 00:10:41 +0000

    first git

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+  $("#sp_navi_btn").click(function () {
+    $(".navi_ul").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
+
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
